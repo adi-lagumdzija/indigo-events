f41e8e1415ea1cde1841275ef177aa9bb2fdb642
dzenimed <dzenana.mededovic@gmail.com>
2022-06-24 16:34:53 +0200

    refactoring

--- rest/config.php
+++ rest/config.php
@@ -12,28 +12,27 @@ class Config {
 
   const SMTP_HOST = "smtp.sendgrid.net";
   const SMTP_PORT = 587;
-  const SMTP_USER = "#";  //DzeniKey
+  const SMTP_USER = "#";
   const SMTP_PASSWORD = "#";
 
 
-
-  // public static function JWT_SECRET(){
-  //   return Config::get_env("JWT_SECRET", "y4KvQcZVqn3F7uxQvcFk");
-  // }
+  public static function JWT_SECRET(){
+    return Config::get_env("JWT_SECRET", "y4KvQcZVqn3F7uxQvcFk");
+  }
 
   // environment servers setup
-      // public static function ENVIRONMENT_SERVER ()
-      // {
-      //     return Config::get_env("ENVIRONMENT_SERVER", "localhost/indigo-events/");
-      // }
-      // public static function PROTOCOL () {
-      //     return strtolower(substr($_SERVER["SERVER_PROTOCOL"],0,strpos( $_SERVER["SERVER_PROTOCOL"],'/'))).'://';
-      // }
-      //
-      // public static function get_env($name, $default)
-      // {
-      //     return isset($_ENV[$name]) && trim($_ENV[$name]) != '' ? $_ENV[$name] : $default;
-      // }
+      public static function ENVIRONMENT_SERVER ()
+      {
+          return Config::get_env("ENVIRONMENT_SERVER", "localhost/indigo-events/");
+      }
+      public static function PROTOCOL () {
+          return strtolower(substr($_SERVER["SERVER_PROTOCOL"],0,strpos( $_SERVER["SERVER_PROTOCOL"],'/'))).'://';
+      }
+
+      public static function get_env($name, $default)
+      {
+          return isset($_ENV[$name]) && trim($_ENV[$name]) != '' ? $_ENV[$name] : $default;
+      }
   // const DB_HOST = "127.0.0.1";
   // const DB_USERNAME = "root";
   // const DB_PASSWORD = "bradi123";
